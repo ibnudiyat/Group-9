8cc8096579a6a309fe37f9c61ec33f273ed6ddf7
AbdirahmanDiyatF <ibnudiyat273@gmail.com>
2023-12-28 20:38:39 +0300

    any message can do

--- Readme.md
+++ Readme.md
@@ -1,10 +1,10 @@
 Challenge 1: Student Grade Generator
 Write a program that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade:
-A > 79
+A > 79 & <= 100
 B - 60 to 79
 C - 59 to 49
 D - 40 to 49
-E - less than 40.
+E - less than 40. & >= 0
 
 
 The calculator is erroneous e.g put 1000 marks.
--- script.js
+++ script.js
@@ -7,19 +7,45 @@ function calculateGrade() {
 
         if (studentMarks >= 79.5 && studentMarks <= 100) {
             grade = "A";
+            var resultBox = document.getElementById("resultBox");
+            resultBox.innerHTML = `${studentName} scored ${studentMarks}, which is grade ${grade}.`;
+
+            
         } else if (studentMarks >= 60 && studentMarks <= 79) {
             grade = "B";
+            var resultBox = document.getElementById("resultBox");
+            resultBox.innerHTML = `${studentName} scored ${studentMarks}, which is grade ${grade}.`;
         } else if (studentMarks >= 50 && studentMarks <= 59) {
             grade = "C";
+            var resultBox = document.getElementById("resultBox");
+            resultBox.innerHTML = `${studentName} scored ${studentMarks}, which is grade ${grade}.`;
         } else if (studentMarks >= 40 && studentMarks <= 49) {
             grade = "D";
-        } else {
-            grade = "E";
+            var resultBox = document.getElementById("resultBox");
+            resultBox.innerHTML = `${studentName} scored ${studentMarks}, which is grade ${grade}.`;
         }
+        else if (studentMarks >= 0 && studentMarks <= 39) {
+            grade = "E";
+            var resultBox = document.getElementById("resultBox");
+            resultBox.innerHTML = `${studentName} scored ${studentMarks}, which is grade ${grade}.`;
 
-        var resultBox = document.getElementById("resultBox");
-        resultBox.innerHTML = `${studentName} scored ${studentMarks}, which is grade ${grade}.`;
+        } else {
+            var resultBox = document.getElementById("resultBox").innerHTML = "";
+            setTimeout(function(){
+                alert(" Please enter a valid numeric value for student marks. ");
+                
+            }, 50);
+        
+            
+            
+            
+        }
     } else {
-        alert("Please enter a valid numeric value for student marks.");
+        var resultBox = document.getElementById("resultBox").innerHTML = "";
+            setTimeout(function(){
+                alert(" Please enter an information in the spaces provided ");
+                
+            }, 50);
     }
 }
+
